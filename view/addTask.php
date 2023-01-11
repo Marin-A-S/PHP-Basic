9673d81aa80ff57b1d3d4e46fe1c0a2603c37233
Marin-A-S <106108224+Marin-A-S@users.noreply.github.com>
2023-01-11 19:54:19 +0300

    lesson-6 homework

---
+++ view/addTask.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<style>
+    <?php include 'addTask.css'; ?>
+</style>
+
+<body>
+    <div class="menu">
+        <a href="/">Main</a>
+        <a href="/?controller=tasks">Tasks</a>
+    </div>
+    <p>User: <?= $username ?>.</p>
+    <div class="taskBox">
+        <p>Add task</p>
+        <form id="add" class="task" action='' method="POST">
+            <input class="inputBox" type="text" name="description" placeholder="Description">
+            <input class="priorityBox" type="number" name="priority" placeholder="Priority">
+            <button form="add" class="taskButton" type="Submit">Add</button>
+        </form>
+    </div>
+</body>
