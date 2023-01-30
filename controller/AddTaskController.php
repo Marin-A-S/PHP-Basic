d83e8821f079624ad2f162d010f266b1423e2eb5
Marin-A-S <106108224+Marin-A-S@users.noreply.github.com>
2023-01-30 04:54:56 +0300

    lesson-7 homework

--- controller/AddTaskController.php
+++ controller/AddTaskController.php
@@ -1,25 +1,29 @@
 <?php
 require_once 'model/Task.php';
 require_once 'model/User.php';
+require_once 'model/TaskProvider.php';
+$pdo = require 'database.php';
 
 session_start();
-
-$username = null;
-
+$userName = null;
 if (isset($_SESSION['username'])) {
-    $username = $_SESSION['username']->getUsername();
-};
+    $userName = $_SESSION['username']->getUsername();
+} else {
+    header('Location: /?controller=index');
+    die();
+}
 
 if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
     if (isset($_POST['description'], $_POST['priority']) && $_POST['description'] !== '') {
         if ($_POST['priority'] == '') {
             $_POST['priority'] = 0;
-        };
-        $task = new Task($_SESSION['username'], $_POST['description'], $_POST['priority']);
-        $_SESSION['tasks'][] = $task;
+        }
+        $task = new Task($_SESSION['username']->getUsername(), $_POST['description'], $_POST['priority']);
+        $taskProvider = new TaskProvider($pdo);
+        $taskProvider->addTask($task);
         header('Location: /?controller=tasks');
         die();
-    };
-};
+    }
+}
 
 include 'view/addTask.php';
--- model/User.php
+++ model/User.php
@@ -1,35 +1,56 @@
 <?php
 
-class User {
+class User
+{
     private string $username;
+    private string $name;
     private string $email;
-    private bool $isActive = true;
-    private ?int $age;
     private ?string $sex;
-    private DateTime $dateOfUserCreation;
+    private ?int $age;
+    private bool $isActive = true;
+    private DateTime $dateCreated;
 
+    // function __construct(string $username, string $email)
     function __construct(string $username)
     {
         $this->username = $username;
-        $this->dateOfUserCreation = new DateTime();
+        // $this->email = $email;
+        $this->dateCreated = new DateTime();
     }
- 
+
     function getUsername(): string
     {
-        return $this->username ?? 'unknown user';
+        return $this->username ?? 'unknown';
+    }
+
+    function setUsername(string $username): self
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    function getName(): string
+    {
+        return $this->name ?? 'unknown';
+    }
+
+    function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
     }
 
     public function setAge(?int $age): void
     {
-       if ($age == null) {
-           $this->age = null;
-       } elseif ($age > 0 && $age <= 125) {
-           $this->age = $age;
-       }
+        if ($age == null) {
+            $this->age = null;
+        } elseif ($age > 0 && $age <= 100) {
+            $this->age = $age;
+        }
     }
 
     public function getAge(): ?int
     {
-       return $this->age;
+        return $this->age;
     }
- }
+}
